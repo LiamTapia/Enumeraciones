96247173f0bf4f9d104fae12f4c931e32d40983c
LiamTapia <liam.tapia@uabc.edu.mx>
2018-08-24 09:55:13 -0700

    Primera version.

---
+++ src/paquete1/Test.java
@@ -0,0 +1,15 @@
+package paquete1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(Operaciones.SUMA);
+		System.out.println(Operaciones.RESTA.name());
+		double a = 2;
+		double b = 4;
+		double res = Operaciones.SUMA.resultado(a,b);
+		System.out.println(res); //patron de diseño state
+
+	}
+
+}
